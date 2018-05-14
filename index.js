8920e1fb12c45f38c4c742c01a64d2aafd3fef05
Unknown <waynewu1229@gmail.com>
2018-05-14 21:20:21 +0800

    complete crawler

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello');
+const Xray = require('x-ray');
+const x = Xray();
+x('https://kiki.ccu.edu.tw/~ccmisp06/Course/5304.html',
+'table > tr', [
+{
+no: 'td:nth-child(2)',
+name: 'td:nth-child(3)',
+teacher: 'td:nth-child(4)',
+},
+]).write('./dist/hello.json');
